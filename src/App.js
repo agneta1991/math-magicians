bd7e19cee5efc0b122a1a541b991596db60e97e9
agneta1991 <pupienytea@gmail.com>
2023-08-10 15:02:26 +0300

    feched data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/calculator';
+import FetchedApi from './components/api';
 
 function App() {
   return (
     <div className="app">
       <Calculator />
+      <FetchedApi />
     </div>
   );
 }
